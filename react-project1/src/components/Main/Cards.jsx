cee01073f5a6877edfe77662373ffe095670ce2f
yejin lee <ingkejin@gmail.com>
2023-03-15 14:54:43 +0900

    ⚡️ map 함수로 Card component 생성

--- react-project1/src/components/Main/Cards.jsx
+++ react-project1/src/components/Main/Cards.jsx
@@ -1,26 +1,33 @@
 import React from 'react'
 import './Cards.css'
 
-const Cards = () => {
+const Cards = (props) => {
+  const data = [
+    {title: 'Title1', content: 'Lorem ipsum dolor sit amet.'},
+    {title: 'Title2', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, modi!'},
+    {title: 'Title3', content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia magnam culpa quisquam voluptate ipsa perspiciatis eligendi corrupti aut nulla non?'},
+  ]
+
   return (
     <section className="post-box">
-      <Card/>
-      <Card/>
-      <Card/>
+      {
+        data.map((item, i)=>{
+          return (
+            <Card title={item.title} content={item.content} key={i}/>
+          )
+        })
+      }
     </section>
   )
 }
 
-function Card(){
+function Card(props){
   return (
     <div className="post-card">
-      <h2>Lorem ipsum dolor sit.</h2>
-      <p className="post-card-txt">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi sint aperiam tempora ratione sunt beatae alias nulla<br/>
-        unde expedita suscipit quisquam natus dolores aliquam, placeat ea iusto, nesciunt, incidunt minima.
-      </p>
+      <h2>{props.title}</h2>
+      <p className="post-card-txt">{props.content}</p>
     </div>
   )
 }
- 
+
 export default Cards
