a83eba93b403476a893b03acbfc6be34bf59791b
yejin lee <ingkejin@gmail.com>
2023-03-15 11:37:13 +0900

    ⚡️ 버튼 클릭시 Alert창 띄우기

--- react-project1/src/components/Main/Main.jsx
+++ react-project1/src/components/Main/Main.jsx
@@ -25,7 +25,11 @@ function Ipsum(props) {
           // showBtn이 true일 때만 button을 렌더링
           // 둘 다 true일 경우에만 버튼을 렌더링하고
           // 좌항이 false일 경우 바로 false를 반환한다
-          showBtn && <button className="front-btn">버튼</button>
+
+          // 이벤트 핸들러에 함수가 아닌 표현식이나 값을 바로 전달할 경우
+          // 컴포넌트가 재렌더링 될 때 마다 해당 값이 실행된다.
+          // 온 클릭 했을 때 실행되게 하려면 이벤트 핸들러에 값을 함수로 전달
+          showBtn && <button className="front-btn" type='button' onClick={()=>alert('안녕하세요!')}>버튼</button>
         }
         
       </div>
